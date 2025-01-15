f5af8b95286edb64a191f67d98bc6cd42885dfff
Quentin-Lecocq <quentin.lecocq@yahoo.com>
2025-01-15 17:43:40 +0100

    implement clerck user button

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,15 @@
+import { UserButton } from '@clerk/nextjs';
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-screen w-screen relative">
       <aside className="absolute top-0 left-0 h-full w-[200px] border-r border-black/10">MOOD</aside>
       <div className="ml-[200px]">
-        <header className="h-[60px] border-b border-black/10">hello</header>
+        <header className="h-[60px] border-b border-black/10">
+          <div className="h-full w-full px-6 flex items-center justify-end">
+            <UserButton />
+          </div>
+        </header>
         <div className="h-[calc(100vh-60px)]">{children}</div>
       </div>
     </div>
