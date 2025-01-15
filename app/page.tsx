f4d3135ea09de9b31722ebb49a625ae6f2eb3cb2
Quentin-Lecocq <quentin.lecocq@yahoo.com>
2025-01-15 17:34:27 +0100

    create first user

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-import Link from 'next/link'
+import { auth } from '@clerk/nextjs/server';
+import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  let href = userId ? '/journal' : '/new-user';
+
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
@@ -9,11 +14,11 @@ export default function Home() {
           This is the best app for tracking your mood trough out your life. All you have to do is be honest.
         </p>
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-blue-600 p-4 rounded-lg text-xl py-2">get started</button>
           </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
