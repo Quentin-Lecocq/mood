d77b43a9937e499dcfe4e5898c64a4d8e1c30697
Quentin-Lecocq <quentin.lecocq@yahoo.com>
2025-01-15 16:59:04 +0100

    configure prisma

--- db/index.ts
+++
@@ -1,9 +0,0 @@
-import { config } from 'dotenv'
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-
-config({ path: '.env.local' })
-
-const client = postgres(process.env.NEXT_PUBLIC_SUPABASE_URL!)
-
-export const db = drizzle(client)
