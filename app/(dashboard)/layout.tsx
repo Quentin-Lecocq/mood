27845e43750706ca7b5ad311dfff8bebec5db82b
Quentin-Lecocq <quentin.lecocq@yahoo.com>
2025-01-15 17:39:38 +0100

    crate layout dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,13 @@
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="h-screen w-screen relative">
+      <aside className="absolute top-0 left-0 h-full w-[200px] border-r border-black/10">MOOD</aside>
+      <div className="ml-[200px]">
+        <header className="h-[60px] border-b border-black/10">hello</header>
+        <div className="h-[calc(100vh-60px)]">{children}</div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
